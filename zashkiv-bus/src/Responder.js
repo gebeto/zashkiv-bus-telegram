c6a1355b7f3ea5045b4f87e1545dbc196e26cbee
gebeto <slavik.nychkalo@gmail.com>
2018-10-02 21:52:12 +0300

    separate bus responder with busesResponser

--- zashkiv-bus/src/Responder.js
+++ zashkiv-bus/src/Responder.js
@@ -1,33 +1,32 @@
 const { googleStatic, googleStreetByLatLong } = require('./Google');
 
+function busResponse(ctx, bus, params) {
+	const caption = `${bus.VehicleName} - ${bus.RouteName}. ${bus.State}`
+	ctx.telegram.sendPhoto(ctx.from.id, googleStatic(bus.Y, bus.X, params), {
+		caption: `${caption}\n\nЗавантаження вулиці...`,
+		parse_mode: null
+	})
+
+	.then((res) => {
+		setTimeout(() => {
+			googleStreetByLatLong(bus.Y, bus.X).then((street) => {
+				ctx.telegram.editMessageCaption(ctx.chat.id, res.message_id, null, `${caption}\n\n${street.formatted_address}`, {
+					parse_mode: null
+				});
+			}).catch(err => {
+				ctx.telegram.editMessageCaption(ctx.chat.id, res.message_id, null, caption, {
+					parse_mode: null
+				});						
+			});
+		}, 3000);
+	});
+}
 
 function busesResponse(ctx, buses = [], params) {
 	return new Promise((resolve, reject) => {
 		for (let i = 0; i < buses.length; i++) {
 			const bus = buses[i];
-			
-			const caption = `${bus.VehicleName} - ${bus.RouteName}. ${bus.State}`
-			ctx.telegram.sendPhoto(ctx.from.id, googleStatic(bus.Y, bus.X, params), {
-				caption: `${caption}\n\nЗавантаження вулиці...`,
-				parse_mode: null
-			})
-			
-			.then((res) => {
-				// console.log(ctx.editMessageText.toString());
-				setTimeout(() => {
-					googleStreetByLatLong(bus.Y, bus.X).then((street) => {
-						ctx.telegram.editMessageCaption(ctx.chat.id, res.message_id, null, `${caption}\n\n${street.formatted_address}`, {
-							parse_mode: null
-						});
-					}).catch(err => {
-						ctx.telegram.editMessageCaption(ctx.chat.id, res.message_id, null, caption, {
-							parse_mode: null
-						});						
-					});
-					// console.log(res);
-					// });
-				}, 3000);
-			});
+			busResponse(ctx, bus, params)
 		}
 	});
 }
