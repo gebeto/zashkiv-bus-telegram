0ed22c1a3ac9c52f1bb2aac9631f87d46f4583bc
gebeto <slavik.nychkalo@gmail.com>
2019-04-27 15:04:11 +0300

    try to add list of busses

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const fetch = require("node-fetch");
 const { Composer } = require("micro-bot")
 const { getParams } = require("./Params");
 const { fetchBusesData, checkDeveloper } = require("./utils");
+const { parse } = require("node-html-parser");
 
 const bot = new Composer()
 
@@ -9,6 +11,17 @@ const bot = new Composer()
 bot.start(({ reply }) => reply("Hey there!"));
 bot.command("help", ({ reply }) => reply("Help message"));
 bot.command("about", ({ reply }) => reply("About message"));
+
+// bot.command("list", async ({ reply }) => {
+// 	const page = await fetch("http://bus.com.ua/cgi-bin/tablo.pl?as=460280").then(res => res.text());
+// 	const root = parse(page);
+// 	const bs = root.querySelectorAll('td');
+// 	console.log(bs);
+// 	const data = bs.map(item => item.text);
+// 	console.log(data);
+// 	// console.log(data);
+// 	return reply(data);
+// });
 
 bot.on("message", async ( ctx ) => {
 	const replyMessage = await ctx.replyWithMarkdown("Зачекайте будь ласка.");
