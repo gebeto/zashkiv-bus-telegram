8f68328aa29f1df003b485d6d439f6ede088eff0
gebeto <slavik.nychkalo@gmail.com>
2019-04-27 13:21:26 +0300

    add tests and fix location

--- src/BusesDataFetcher.js
+++ src/BusesDataFetcher.js
@@ -1,48 +1,59 @@
 const fetch = require('node-fetch');
 
-class BusesData {
-	constructor(json) {
-		// console.log(typeof json);
-		if (typeof json === 'string') {
-			this.data = JSON.parse(json);
-		} else if (typeof json === 'object') {
-			this.data = json;
-		} else {
-			this.data = {};
-		}
-	}
+
+async function fetchBusesData() {
+	const response = await fetch('http://82.207.107.126:13541/tms/loda/api/389C2151-C5E9-4D47-8B94-5F643CA0AFEB/GetRouteInfo?routeId=1059838');
+	const json = await response.json();
+	return json.Data;
 }
 
-function fetchBusesData() {
-	// http://82.207.107.126:13541/tms/loda/api/389C2151-C5E9-4D47-8B94-5F643CA0AFEB/GetRouteInfo?routeId=1059838
-	// return fetch('http://82.207.107.126:13541/SimpleRide/LODA/SM.WebApi/api/RouteMonitoring/?code=LODA|1059838')
-	return fetch('http://82.207.107.126:13541/tms/loda/api/389C2151-C5E9-4D47-8B94-5F643CA0AFEB/GetRouteInfo?routeId=1059838')
-		.then(res => res.json()).then(res => {
-			const result = new BusesData(res);
-			if (result.data.length === 0) {
-				throw new Error('Список автобусів пустий!');
-			}
-			return result;
-		});
-}
 
 exports.fetchBusesData = fetchBusesData;
 
-// [{
-// 	"Angle": 50.273394890841558,
-// 	"EndPoint": "",
-// 	"IterationEnd": null,
-// 	"IterationStart": null,
-// 	"RouteCode": null,
-// 	"RouteId": 1059838,
-// 	"RouteName": "165, Львів АС-2 - Зашків - Завадів",
-// 	"StartPoint": "",
-// 	"State": 1,
-// 	"TimeToPoint": 5040,
-// 	"VehicleId": 42632,
-// 	"VehicleName": "ВС 1759 АА",
-// 	"X": 24.057065,
-// 	"Y": 49.8694366,
-// 	"LowFloor": false,
-// 	"ActualOnDate": "2018-03-20T15:45:56"
-// }]
+
+/*
+{
+	"Exception": null
+	"$id": "1",
+	"Data": [
+		{
+			"$id": "2",
+			"Angle": -5.943510254110893,
+			"EndPoint": "",
+			"IterationEnd": null,
+			"IterationStart": null,
+			"RouteCode": null,
+			"RouteId": 1059838,
+			"RouteName": "165, Львів АС-2 - Зашків - Завадів",
+			"StartPoint": "",
+			"State": 2,
+			"TimeToPoint": -5041,
+			"VehicleId": 37665,
+			"VehicleName": "ВС 1090 АА",
+			"X": 23.9313508,
+			"Y": 49.8258818,
+			"LowFloor": false,
+			"ActualOnDate": "2019-04-27T12:37:59"
+		},
+		{
+			"$id": "3",
+			"Angle": -142.40372885287175,
+			"EndPoint": "",
+			"IterationEnd": null,
+			"IterationStart": null,
+			"RouteCode": null,
+			"RouteId": 1059838,
+			"RouteName": "165, Львів АС-2 - Зашків - Завадів",
+			"StartPoint": "",
+			"State": 0,
+			"TimeToPoint": -1464,
+			"VehicleId": 42632,
+			"VehicleName": "ВС 1759 АА",
+			"X": 24.0154166,
+			"Y": 49.8427933,
+			"LowFloor": false,
+			"ActualOnDate": "2019-04-27T12:38:41"
+		}
+	]
+}
+*/
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { Composer, log } = require('micro-bot')
 const { getParams } = require('./Params');
 const { googleStatic } = require('./Google');
 const { busResponse } = require('./Responder');
-const fetchBusesData = require('./BusesDataFetcher').fetchBusesData;
+const { fetchBusesData } = require('./BusesDataFetcher');
 
 
 const bot = new Composer()
@@ -15,6 +15,7 @@ function checkDeveloper(ctx) {
 	}
 }
 
+
 // bot.use(log());
 bot.start(({ reply }) => reply('Hey there!'));
 bot.command('help', ({ reply }) => reply('Help message'));
@@ -22,16 +23,14 @@ bot.command('about', ({ reply }) => reply('About message'));
 
 bot.on('message', async ( ctx ) => {
 	const replyMessage = await ctx.replyWithMarkdown('Зачекайте будь ласка.');
-	const busesData = await fetchBusesData();
+	const buses = await fetchBusesData();
 	const params = getParams(ctx.message.text.toLowerCase());
-	const buses = busesData.data.Data;
-	buses.forEach(async (bus) => {
-		try {
-			const res = await busResponse(ctx, bus, params);
-			console.log(res);
-		} catch(err) {
-			await ctx.replyWithMarkdown(err);
-		}
+	buses.forEach((bus) => {
+		ctx.replyWithVenue(bus.Y, bus.X, bus.VehicleName, bus.RouteName, {
+			// foursquare_type: "bus_line"
+			foursquare_id: "4bf58dd8d48988d12b951735"
+			// foursquare_type: "food"
+		});
 	});
 	// ctx.telegram.sendMessage(ctx.from.id, `Помилка: ${err.message}`);
 });
